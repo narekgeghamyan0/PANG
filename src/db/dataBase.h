e4fd1ad2b745a2bba7968db4bf4d4ea377edd64d
Andranik Pashayan <pashayan2000@gmail.com>
2026-01-24 11:26:42 +0400

    Added sql commands creators

--- src/db/dataBase.h
+++ src/db/dataBase.h
@@ -1,27 +1,67 @@
+#ifndef DB_DATA_BASE_H
+#define DB_DATA_BASE_H
+
 #include <string>
 #include <iostream>
 #include <sqlite3.h>
 
+#define QUERY_MAX_SIZE 256
+
 using string = std::string;
-using ID     = size_t;
+using ID = size_t;
+using SQL_string = std::string;
 
-class DataBase {
+// SQL Keywords
+// TODO: Create queries using these keywords
+const SQL_string SQL_CREATE = "CREATE";
+const SQL_string SQL_TABLE = "TABLE";
+const SQL_string SQL_IF = "IF";
+const SQL_string SQL_EXISTS = "EXISTS";
+const SQL_string SQL_OPEN_BRACKET = "(";
+const SQL_string SQL_CLOSE_BRACKET = ")";
+const SQL_string SQL_INTEGER = "INTEGER";
+const SQL_string SQL_PRIMARY = "PRIMARY";
+const SQL_string SQL_KEY = "KEY";
+const SQL_string SQL_AUTOINCREMENT = "AUTOINCREMENT";
+const SQL_string SQL_COMMA = ",";
+const SQL_string SQL_TEXT = "TEXT";
+const SQL_string SQL_NOT = "NOT";
+const SQL_string SQL_NULL = "NULL";
+const SQL_string SQL_ASTERICS = "*";
+const SQL_string SQL_EQ = "=";
+const SQL_string SQL_SELECT = "SELECT";
+const SQL_string SQL_FROM = "FROM";
+const SQL_string SQL_WHERE = "WHERE";
+const SQL_string SQL_INSERT = "INSERT";
+const SQL_string SQL_INTO = "INTO";
+const SQL_string SQL_DELETE = "DELETE";
+const SQL_string SQL_VALUES = "VALUES";
+const SQL_string SQL_SEMICOLON = ";";
+
+class DataBase
+{
 public:
-    DataBase(const string& db_path);
+    DataBase(const string &db_path);
     ~DataBase();
-    // const string& getImagePath(ID id) const;
-    // const string& getNote(ID id) const;
-    // void insertNoteAndImage(ID id, string& image_path, string& note);
+    void insert(const string &tableName, ID id, const string &image_path, const string &note);
+    void erase(const string &tableName, ID id);
+    string getImagePath(ID id);
+    string getNote(ID id);
 
 private:
-    void createDataBase(const string& db_path);
-    void executeCommand(const string& command);
-    void createTable(string&& user);
+    void createDataBase(const string &db_path);
+    void executeCommand(const string& command, sqlite3_stmt* stmt = nullptr);
+    void createTable(string &&user);
     void createMemoryTable();
-    void createQuery(string&& item, string&& column, ID id, string& query);
-    void createQueryForImage(ID id, string& query);
-    void createQueryForNote(ID id, string& query);
+    void createPrintAllQuery(const string &tableName, string &query);
+    void createSelectQuery(string &&item, string &&column, ID id, string &query);
+    void createQueryForImage(ID id, string &query);
+    void createQueryForNote(ID id, string &query);
+    void createInsertQuery(const string &tableName, ID id, const string &image_path, const string &note, string &query);
+    void createEraseQuery(const string &tableName, ID id, string &query);
 
 private:
-    sqlite3* db_;
+    sqlite3 *db_;
 };
+
+#endif // DB_DATA_BASE_H
